b1046bb2f315779c0ca5157979c9d4008020e1a6
Ira <93600239+Renie56@users.noreply.github.com>
2022-05-01 16:28:52 +0300

    Update Задача 1.py

--- Задача 1.py
+++ Задача 1.py
@@ -1,8 +1,11 @@
 import math
 def main(v, l, x1):
     v = math.sqrt(2*g*(y0-math.sqrt((r**2)-(x0**2))))
-    l = math.asin(x/r)
-    parabola(v, l, x)
+    l = math.asin(x0/r)
+    print(v, l)
+    x1 = parabola(v, l, x)
+    print(x1)
+    return x1
 def parabola(V, L, X):
     '''y1 = abs((((r**2)-(x0**2))**0.5+(X*x0)*(1/math.tan(2*L))-(g*((X-x0)**2))/2*(V**2)*((math.sin(2*L)**2)))-(((r**2)-(X**2))**0.5))
     while round(y1) != 0:
@@ -19,4 +22,6 @@ g = 9.8
 v0 = 0
 l0 = 0
 c = 0
-print(main(v0, l0, x))
+for c in range(0, 3):
+    x = main(v0, l0, x0)
+    x0 = x
