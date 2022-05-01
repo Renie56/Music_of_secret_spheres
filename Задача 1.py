ed8074cd4f960ef65d2fd1a93ef8270beee61a90
Ira <93600239+Renie56@users.noreply.github.com>
2022-05-01 15:48:41 +0300

    Update Задача 1.py

--- Задача 1.py
+++ Задача 1.py
@@ -1,3 +1,22 @@
-x = int(input())
-y = int(input())
+import math
+def main(v, l, x1):
+    v = (2*g*(y0-math.sqrt((r**2)*(x0**2))))**0.5
+    l = math.asin(x/r)
+    parabola(v, l, x)
+def parabola(V, L, X):
+    '''y1 = abs((((r**2)-(x0**2))**0.5+(X*x0)*(1/math.tan(2*L))-(g*((X-x0)**2))/2*(V**2)*((math.sin(2*L)**2)))-(((r**2)-(X**2))**0.5))
+    while round(y1) != 0:
+        X+=1
+        y1 = abs((((r**2)-(x0**2))**0.5+(X*x0)*(1/math.tan(2*L))-(g*((X-x0)**2))/2*(V**2)*((math.sin(2*L)**2)))-(((r**2)-(X**2))**0.5))
+        print(X)''' #Пошук координати x
+    return X
+x0 = 20
+y0 = 300
+x = x0
 y = 0
+r = 150
+g = 9.8
+v0 = 0
+l0 = 0
+c = 0
+print(main(v0, l0, x))
