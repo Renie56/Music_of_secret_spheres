a91cf98b075d9b5449b886ec55d93eded0dcd31b
Ira <93600239+Renie56@users.noreply.github.com>
2022-05-02 23:14:17 +0300

    Update Задача 1.py

--- Задача 1.py
+++ Задача 1.py
@@ -3,27 +3,30 @@ import turtle
 def main():
     global v0, l0, x0, x
     v0 = (2*g*(y0-math.sqrt((r**2)-(x0**2))))**0.5
-    #print(v)
-    l0 = math.asin(x0/r)
-    #print(v, math.degrees(l))
+    if c == 0:
+        l0 = math.asin(x0/r)
+    else:
+        b = -x0/(((r**2)-(x0**2))**0.5)
+        u = (1/math.tan(2*l0))-(g*(x0-x1)/((v0**2)*(math.sin(2*l0)**2)))
+        l0 = 180 + b - 2*u
     parabola()
     print('parabola[X] =', x)
 def parabola():
     global v0, l0, x, Left, Right
-    Left = (v0**2)*math.sin(4*l0)/(g*2)+x0
-    Right = r
-    print('Le:', Left, 'Ri:', Right)
     x = None
-    while Left<=Right:
-        x = (Left+Right)//2
-        y = ((((r**2)-(x0**2))**0.5+(x-x0)*(1/math.tan(2*l0))-(g*((x-x0)**2))/(2*(v0**2)*((math.sin(2*l0)**2))))-(((r**2)-(x**2))**0.5))
-        print('y1:', y,'Le:', Left, 'Ri:', Right, 'X:', x)
-        if (int(y) == 0 or math.ceil(y) == 0):
-            break
-        elif y<0:
-            Right = x-1
-        else:
-            Left = x+1
+    if math.sin(4*l0)>0:
+        Left = (v0**2)*math.sin(4*l0)/(g*2)+x0
+        Right = r
+        print('Le:', Left, 'Ri:', Right)
+        while Left<=Right:
+            x = (Left+Right)/2
+            y = ((((r**2)-(x0**2))**0.5+(x-x0)*(1/math.tan(2*l0))-(g*((x-x0)**2))/(2*(v0**2)*((math.sin(2*l0)**2))))-(((r**2)-(x**2))**0.5))
+            if (int(y) == 0 or math.ceil(y) == 0):
+                break
+            elif y<0:
+                Right = x-0.01
+            else:
+                Left = x+0.01
     graf_parabola()
 def grafics():
     global x0, y0
@@ -32,9 +35,9 @@ def grafics():
     turtle.down()
     turtle.circle(1)
     turtle.up()
-    turtle.goto(0, -150)
+    turtle.goto(0, -r)
     turtle.down()
-    turtle.circle(150)
+    turtle.circle(r)
     turtle.up()
     turtle.right(90)
     turtle.goto(0, 0)
@@ -51,19 +54,19 @@ def graf_parabola():
         turtle.down()
         x1+=1
 x0 = 1
-y0 = 300
+y0 = 297
 x = 0
 r = 150
-g = 9800
+g = 1
 v0 = 0
 l0 = 0
 c = 0
 Left = x0
 Right = r
 grafics()
-for c in range(0, 3):
+for c in range(0, 4):
+    x1 = x0
     main()
-    print('x:', x, 'x0:', x0)
     x0 = x
-    print('x:', x, 'x0:', x0)
 turtle.mainloop()
+
