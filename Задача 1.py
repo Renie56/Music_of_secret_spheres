2402bcd5fbbede1b4535a87b6b97d3b7235f4c44
Ira <93600239+Renie56@users.noreply.github.com>
2022-05-02 13:53:03 +0300

    Update Задача 1.py

--- Задача 1.py
+++ Задача 1.py
@@ -1,62 +1,69 @@
 import math
 import turtle
-def main(v, l, x1):
-    v = (2*g*(y0-math.sqrt((r**2)-(x0**2))))**0.5
-    print("v = ", v/1000)
-    l = math.asin(x0/r)
-    x1 = parabola(v, l, x, Left, Right)
-    print('parabola[X] =', x1)
-    return x1
-def parabola(V, L, X, Le, Ri):
-    X = x0
-    y1 = ((((r ** 2) - (x0 ** 2)) ** 0.5 + (X - x0) * (1 / math.tan(2 * L)) - (g * ((X - x0) ** 2)) / (2 * (V ** 2) * ((math.sin(2 * L) ** 2)))))
-    while X < 250 and y1 > 0:
-        y1 = ((((r ** 2) - (x0 ** 2)) ** 0.5 + (X - x0) * (1 / math.tan(2 * L)) - (g * ((X - x0) ** 2)) / (
-                    2 * (V ** 2) * ((math.sin(2 * L) ** 2)))))
-        #print(y1, X)  # Пошук координати x(перебор)
-        turtle.goto(X, y1)
-        turtle.down()
-        X+=1
-
-
-
-    ##################бінарний пошук
-    Le = (V**2)*(math.sin(4*L))/(g*2)+x0
-    while Le<=Ri:
-        X = (Le+Ri)/2
-        y1 = ((((r**2)-(x0**2))**0.5+(X-x0)*(1/math.tan(2*L))-(g*((X-x0)**2))/(2*(V**2)*((math.sin(2*L)**2))))-(((r**2)-(X**2))**0.5))
-        if (int(y1) == 0 or math.ceil(y1) == 0):
-            return X
-        elif y1<0:
-            Ri = X - 1
+def main():
+    global v0, l0, x0, x
+    v0 = (2*g*(y0-math.sqrt((r**2)-(x0**2))))**0.5
+    #print(v)
+    l0 = math.asin(x0/r)
+    #print(v, math.degrees(l))
+    parabola()
+    print('parabola[X] =', x)
+def parabola():
+    global v0, l0, x, Left, Right
+    Left = (v0**2)*math.sin(4*l0)/(g*2)+x0
+    Right = r
+    print('Le:', Left, 'Ri:', Right)
+    while Left<=Right:
+        x = (Left+Right)//2
+        y = ((((r**2)-(x0**2))**0.5+(x-x0)*(1/math.tan(2*l0))-(g*((x-x0)**2))/(2*(v0**2)*((math.sin(2*l0)**2))))-(((r**2)-(x**2))**0.5))
+        print('y1:', y,'Le:', Left, 'Ri:', Right, 'X:', x)
+        if (int(y) == 0 or math.ceil(y) == 0):
+            graf_parabola()
+            break
+        elif y<0:
+            Right = x-1
         else:
-             Le = X + 1 #Пошук координати х(бінарний пошук(не робочий, кста))
-    return X
-
-
-x0 = 13
+            Left = x+1
+    #return x
+def grafics():
+    global x0, y0
+    turtle.up()
+    turtle.goto(x0, y0 - 1)
+    turtle.down()
+    turtle.circle(1)
+    turtle.up()
+    turtle.goto(0, -150)
+    turtle.down()
+    turtle.circle(150)
+    turtle.up()
+    turtle.right(90)
+    turtle.goto(0, 0)
+    turtle.down()
+    turtle.circle(1)
+    turtle.up()
+def graf_parabola():
+    global l0, v0, y
+    x1 = x0
+    y = ((((r ** 2) - (x0 ** 2)) ** 0.5 + (x1 - x0) * (1 / math.tan(2 * l0)) - (g * ((x1 - x0) ** 2)) / (2 * (v0 ** 2) * ((math.sin(2 * l0) ** 2)))))
+    while x1 <= x and y > 0:
+        y = ((((r ** 2) - (x0 ** 2)) ** 0.5 + (x1 - x0) * (1 / math.tan(2 * l0)) - (g * ((x1 - x0) ** 2)) / (2 * (v0 ** 2) * ((math.sin(2 * l0) ** 2)))))
+        turtle.goto(x1, y)
+        turtle.down()
+        x1+=1
+x0 = 1
 y0 = 300
-turtle.up()
-turtle.goto(x0, y0 - 1)
-turtle.down()
-turtle.circle(1)
-turtle.up()
 x = 0
 r = 150
 g = 9800
 v0 = 0
 l0 = 0
 c = 0
-z = 0
 Left = x0
 Right = r
-turtle.up()
-turtle.goto(0, -150)
-turtle.down()
-turtle.circle(150)
-turtle.up()
-turtle.right(90)
+grafics()
 for c in range(0, 3):
-    x = main(v0, l0, x0)
+    main()
+    print('x:', x, 'x0:', x0)
     x0 = x
+    print('x:', x, 'x0:', x0)
 turtle.mainloop()
