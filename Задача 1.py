0b7a918cde3242ef7eb06da5944b29f00df0010c
Ira <93600239+Renie56@users.noreply.github.com>
2022-05-06 13:21:46 +0300

    Update Задача 1.py

--- Задача 1.py
+++ Задача 1.py
@@ -10,18 +10,18 @@ def main():
         u = (1/math.tan(2*l0))-(g*(x0-x1)/((v0**2)*(math.sin(2*l0)**2)))
         l0 = 180 + b - 2*u
     parabola()
-    print('parabola[X] =', x)
+    #print('parabola[X] =', x)
 def parabola():
     global v0, l0, x, Left, Right, q
     x = None
     if math.sin(4*l0)>0:
         Left = (v0**2)*math.sin(4*l0)/(g*2)+x0
         Right = r
-        print('Le:', Left, 'Ri:', Right)
+        #print('Left:', Left, 'Right:', Right)
         while Left<=Right and (x==None or x<r):
             x = (Left+Right)/2
             y = ((((r**2)-(x0**2))**0.5+(x-x0)*(1/math.tan(2*l0))-(g*((x-x0)**2))/(2*(v0**2)*((math.sin(2*l0)**2))))-(((r**2)-(x**2))**0.5))
-            print(y)
+            #print(y)
             if (int(y) == 0 or math.ceil(y) == 0):
                 q+=1
                 '''turtle.showturtle()
@@ -54,41 +54,51 @@ def graf_parabola():
     global l0, v0, y
     x1 = x0
     y = ((((r ** 2) - (x0 ** 2)) ** 0.5 + (x1 - x0) * (1 / math.tan(2 * l0)) - (g * ((x1 - x0) ** 2)) / (2 * (v0 ** 2) * ((math.sin(2 * l0) ** 2)))))
-    print('Кінець параболи:', x1, round(x, 2))
+    #print('Кінець параболи:', x1, round(x, 2))
     while x1 <= round(x, 2) and y >= 0:
         y = ((((r ** 2) - (x0 ** 2)) ** 0.5 + (x1 - x0) * (1 / math.tan(2 * l0)) - (g * ((x1 - x0) ** 2)) / (2 * (v0 ** 2) * ((math.sin(2 * l0) ** 2)))))
         turtle.goto(x1, y)
         turtle.down()
         x1+=0.1
-def borders:
-    global x0, y0
+def borders(a):
+    global x0, y0, x, q, Left, Right
     Le = w
     Ri = p
-    x0 = (w+p)/2
-    while Le
+    Left = x0
+    Right = r
+    while Le!=Ri and Le<Ri:
+        x00 = (Le+Ri)/2       
+        x0 = x00
+        q = 1
         while x!=None and x<r:
             x1 = x0
             main()
             x0 = x
-        if q>=4:
-            le = x0
-        elif q<=3:
-            Ri = x0
-print(q)
+        x = 0
+        if q>=3+a:
+            Le = x00+0.0000001
+        else:
+            Ri = x00-0.0000001
+    return Ri
 q = 1
 w = 0.001415
 p = 1.6
-x0 = 0.001415
+x0 = 0
 y0 = 300
 x = 0
 r = 150
-a = 400
-g = 1
+g = 9.8
 v0 = 0
 l0 = 0
 c = 0
+mx = 0
+mn = 0
 Left = x0
 Right = r
+while y0<=1000:
+    mx = borders(0)
+    mn = borders(1)
+    print('Висота:', y0, 'Від:', mn, 'До:', mx)
+    y0+=10
 #grafics()
-
 #turtle.mainloop()
