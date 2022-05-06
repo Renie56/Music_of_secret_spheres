2f48e42993d83a0ae8c425b28dd838e92b01aacd
Ira <93600239+Renie56@users.noreply.github.com>
2022-05-06 20:22:37 +0300

    Update Задача 1.py

--- Задача 1.py
+++ Задача 1.py
@@ -3,12 +3,20 @@ import turtle
 def main():
     global v0, l0, x0, x
     v0 = (2*g*(y0-math.sqrt((r**2)-(x0**2))))**0.5
-    if c == 0:
+    if q == 1:
         l0 = math.asin(x0/r)
     else:
         b = -x0/(((r**2)-(x0**2))**0.5)
+        '''turtle.up()
+        for i in range(0, 100):
+            b = -i/(((r**2)-(i**2))**0.5)
+            turtle.goto(i, b)
+            turtle.down()
+        turtle.up()'''
         u = (1/math.tan(2*l0))-(g*(x0-x1)/((v0**2)*(math.sin(2*l0)**2)))
-        l0 = 180 + b - 2*u
+        l0 = math.radians(180) + math.atan(b) - math.atan(2*u)
+    print('Швидкість:', v0)
+    print('кут:', math.degrees(l0))
     parabola()
     print('parabola[X] =', x)
 def parabola():
@@ -20,18 +28,18 @@ def parabola():
         print('Le:', Left, 'Ri:', Right)
         while Left<=Right and (x==None or x<r):
             x = (Left+Right)/2
-            y = ((((r**2)-(x0**2))**0.5+(x-x0)*(1/math.tan(2*l0))-(g*((x-x0)**2))/(2*(v0**2)*((math.sin(2*l0)**2))))-(((r**2)-(x**2))**0.5))
+            y = (((((r**2)-(x0**2))**0.5+(x-x0)*(1/math.tan(2*l0))-(g*((x-x0)**2))/(2*(v0**2)*((math.sin(2*l0)**2)))))-(((r**2)-(x**2))**0.5))
             print(y)
             if (int(y) == 0 or math.ceil(y) == 0):
                 q+=1
-                '''turtle.showturtle()
-                graf_parabola()
-                turtle.hideturtle()'''
                 break
             elif y<0:
                 Right = x-0.0000001
             else:
                 Left = x+0.0000001
+        turtle.showturtle()
+        graf_parabola()
+        turtle.hideturtle()
 def grafics():
     global x0, y0
     turtle.up()
@@ -54,16 +62,18 @@ def graf_parabola():
     global l0, v0, y
     x1 = x0
     y = ((((r ** 2) - (x0 ** 2)) ** 0.5 + (x1 - x0) * (1 / math.tan(2 * l0)) - (g * ((x1 - x0) ** 2)) / (2 * (v0 ** 2) * ((math.sin(2 * l0) ** 2)))))
-    print('Кінець параболи:', x1, round(x, 2))
-    while x1 <= round(x, 2) and y >= 0:
+    print('Кінець параболи:', x1, x)
+    while x1 <= x and y >= 0:
         y = ((((r ** 2) - (x0 ** 2)) ** 0.5 + (x1 - x0) * (1 / math.tan(2 * l0)) - (g * ((x1 - x0) ** 2)) / (2 * (v0 ** 2) * ((math.sin(2 * l0) ** 2)))))
         turtle.goto(x1, y)
         turtle.down()
         x1+=0.1
 q = 1
-x0 = 0.06721064110713007
-y0 = 1000
+d = 40.3
+x0 = 0.9
+y0 = 300
 x = 0
+a = 400
 r = 150
 g = 9.8
 v0 = 0
@@ -71,11 +81,20 @@ l0 = 0
 c = 0
 Left = x0
 Right = r
-#grafics()
+grafics()
 while x!=None and x<r:
     print(x0)
     x1 = x0
     main()
     x0 = x
+    '''turtle.up()
+    turtle.goto(x0, r)
+    turtle.down()
+    turtle.goto(x0+10, r)
+    turtle.up()
+    turtle.goto(x0, r)
+    turtle.down()
+    turtle.goto(x0, 0)
+    turtle.up()'''
 print('q:', q)
-#turtle.mainloop()
+turtle.mainloop()
